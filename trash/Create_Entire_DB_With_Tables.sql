5953743c385cf218de0a0bf624fe8491b4dccccf
Matthew J. Lyons <lyonsmj@rose-hulman.edu>
2019-02-14 00:38:06 -0500

    Started on file to create everything ever

--- trash/Create_Entire_DB_With_Tables.sql
+++ trash/Create_Entire_DB_With_Tables.sql
@@ -1,19 +1,17 @@
---DROP DATABASE [ChristmasWorkshop]
-
---CREATE DATABASE [ChristmasWorkshop]
---ON
---( NAME = [ChristmasWorkshop],
---  FILENAME = 'E:\Database\MSSQL12.MSSQLSERVER\MSSQL\DATA\ChristmasWorkshop.mdf',
---  SIZE = 10MB,
---  MAXSIZE = 420MB,
---  FILEGROWTH = 6% )
---LOG ON
---( NAME = [ChristmasWorkshop_log],
---  FILENAME = 'E:\Database\MSSQL12.MSSQLSERVER\MSSQL\DATA\ChristmasWorkshop_log.ldf',
---  SIZE = 5MB,
---  MAXSIZE = 69MB,
---  FILEGROWTH = 9% ) ;
---GO
+CREATE DATABASE [ChristmasWorkshop]
+ON
+( NAME = [ChristmasWorkshop],
+  FILENAME = 'E:\Database\MSSQL12.MSSQLSERVER\MSSQL\DATA\ChristmasWorkshop.mdf',
+  SIZE = 10MB,
+  MAXSIZE = 420MB,
+  FILEGROWTH = 6% )
+LOG ON
+( NAME = [ChristmasWorkshop_log],
+  FILENAME = 'E:\Database\MSSQL12.MSSQLSERVER\MSSQL\DATA\ChristmasWorkshop_log.ldf',
+  SIZE = 5MB,
+  MAXSIZE = 69MB,
+  FILEGROWTH = 9% ) ;
+GO
 
 USE ChristmasWorkshop
 GO
@@ -126,3 +124,4 @@ Create Table [dbo].[Ohoho](
 	Primary Key(id),
 	Foreign Key (Santa) References Santa(id)
 );
+GO
